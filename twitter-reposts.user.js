a1c1e2636cceef7da737d8421fc97140dd7d78ea
friendlyanon <friendlyanon@users.noreply.github.com>
2026-05-12 02:36:07 +0200

    Update to r4

--- twitter-reposts.user.js
+++ twitter-reposts.user.js
@@ -6,7 +6,7 @@
 // @match       https://x.com/*
 // @grant       none
 // @run-at      document-start
-// @version     3
+// @version     4
 // @downloadURL https://raw.githubusercontent.com/friendlyanon/twitter-reposts/master/twitter-reposts.user.js
 // @supportURL  https://github.com/friendlyanon/twitter-reposts/issues
 // @homepageURL https://github.com/friendlyanon/twitter-reposts
